aea247e28dbff9611b8bdf86df8817456db2af53
oyuyump <yuyumpoptec@gmail.com>
2024-09-06 19:48:04 -0600

    Add CustomPageControl with logo WIP

--- GOW_Localization/Controller/GamesViewController.swift
+++ GOW_Localization/Controller/GamesViewController.swift
@@ -7,18 +7,22 @@
 
 import UIKit
 
-class GamesViewController: UIViewController {
+class GamesViewController: UIViewController, UIScrollViewDelegate {
 
-    @IBOutlet weak var gamePosterPageControl: UIPageControl!
+    @IBOutlet weak var gamePosterPageControl: CustomPageControl!
     @IBOutlet weak var gamePosterImage: UIImageView!
     let gamePosters = Array(0...7)
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
         gamePosterPageControl.numberOfPages = gamePosters.count
+        gamePosterPageControl.setupCustomDots()
+        gamePosterPageControl.currentPage = 0
         gamePosterImage.image = UIImage(named: String(gamePosters.first!))
+        
     }
     
 
@@ -26,27 +30,46 @@ class GamesViewController: UIViewController {
         print("Right")
         if gamePosterPageControl.currentPage == gamePosters.count-1  {
                     gamePosterPageControl.currentPage = 0
+        
                     gamePosterImage.image = UIImage(named: String(gamePosters[gamePosterPageControl.currentPage]))
+            gamePosterPageControl.updateCurrentPage()
                 }
                 else{
                     gamePosterPageControl.currentPage = gamePosterPageControl.currentPage + 1
                     gamePosterImage.image = UIImage(named: String(gamePosters[gamePosterPageControl.currentPage]))
+                    gamePosterPageControl.updateCurrentPage()
                 }
         
     }
     @IBAction func rightSwipeDone(_ sender: Any) {
         print("Left")
-        
-        
         if gamePosterPageControl.currentPage == 0  {
                     gamePosterPageControl.currentPage = gamePosters.count-1
                     gamePosterImage.image = UIImage(named: String(gamePosters[gamePosterPageControl.currentPage]))
+            gamePosterPageControl.updateCurrentPage()
                 }
                 else{
                     gamePosterPageControl.currentPage = gamePosterPageControl.currentPage - 1
                     gamePosterImage.image = UIImage(named: String(gamePosters[gamePosterPageControl.currentPage]))
+                    gamePosterPageControl.updateCurrentPage()
                 }
     }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gamePosterPageControl.setNeedsLayout() // Asegura que layoutSubviews se ejecute
+    }
+
+    
+    /*
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            let pageIndex = Int(round(scrollView.contentOffset.x / scrollView.frame.width))
+            gamePosterPageControl.currentPage = pageIndex
+        gamePosterPageControl.updateCurrentPage()
+        }
+     */
     /*
     // MARK: - Navigation
 
